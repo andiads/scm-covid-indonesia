c354c793df1d88b91fa0f1b8a98ab42a2c1e80fb
Andi Dwi <61771890+andiads@users.noreply.github.com>
2020-05-12 15:38:26 +0700

    Update litbangkesdb-struct.sql

--- litbangkesdb-struct.sql
+++ litbangkesdb-struct.sql
@@ -3,14 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2020 at 08:14 AM
+-- Generation Time: May 12, 2020 at 10:37 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -273,8 +272,8 @@ CREATE TABLE `user` (
   `last_login` datetime DEFAULT NULL,
   `last_ip` varchar(20) DEFAULT NULL,
   `status` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
