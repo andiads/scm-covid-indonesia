e5195ef3d490b218f05431f4baed0d857bb22b55
Andi Dwi <61771890+andiads@users.noreply.github.com>
2020-05-12 18:06:18 +0700

    Update MvcConfig.java

--- src/main/java/com/nusacamp/app/config/MvcConfig.java
+++ src/main/java/com/nusacamp/app/config/MvcConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// TODO Auto-generated method stub
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/home").setViewName("index");
+		registry.addViewController("/").setViewName("index");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/api/v1/login").setViewName("login");
 	}
